a59f475f279ec8aa59a65c0d505686362065e796
Jeongsunga <jsa3341@gmail.com>
2024-08-22 00:39:52 +0900

    making UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom"
+import ProductAll from './Pages/ProductAll';
+import Login from './Pages/Login';
+import ProductDetail from './Pages/ProductDetail';
+import Navbar from './component/Navbar';
 
+// 전체 상품 페이지, 로그인 페이지, 상품 상세 페이지
+// 1. 전체 상품 페이지에서는 전체 상품을 볼 수 있다.
+// 2. 로그인 버튼을 누르면 로그인 페이지가 나온다.
+// 3-1. 상품 디테일을 눌렀으나, 로그인이 안 되어 있을 경우 로그인 페이지가 먼저 나온다.
+// 3-2. 로그인이 되어 있을 경우에는 상품 상세 페이지를 볼 수 있다.
+// 4. 로그아웃 버튼을 누르면 로그아웃이 된다.
+// 5. 로그아웃 되면 상품 상세 페이지를 볼 수 없다. 다시 로그인 페이지로 돌아온다.
+// 6. 상품을 검색할 수 있다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="" element={<ProductAll/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/product/:id" element={<ProductDetail/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/component/ProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src="https://noona-hnm.netlify.app/pattern-jacket.jpeg" alt=""/>
+        <div>벨티드 트윌 코트</div>
+        <div>99900</div>
+        <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
